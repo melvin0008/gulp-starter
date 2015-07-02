a77ee21683f7a94c90586f8b3e90a40c3913685c
Melvin <melvin0008@gmail.com>
2015-07-03 03:16:38 +0530

    added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
+    plumber = require('gulp-plumber');
+    browserSync = require('browser-sync');
+    reload      = browserSync.reload;
 
 gulp.task('scripts',function(){
 	gulp.src(['app/js/**/*.js','!app/js/**/*.min.js'])
+	.pipe(plumber())
 	.pipe(rename({suffix:'.min'}))
 	.pipe(uglify())
 	.pipe(gulp.dest('app/js'));
 });
 
+gulp.task('html', function(){
+    gulp.src('app/**/*.html')
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./app/"
+        }
+    });
+});
+
 
 gulp.task ('watch', function(){
 	gulp.watch('app/js/**/*.js', ['scripts']);
+	gulp.watch('app/**/*.html', ['html']);
 });
 
-gulp.task('default',['scripts','watch']);
+gulp.task('default',['scripts','html','browser-sync','watch']);
